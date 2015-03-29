667cde9e08be9f95e76019e118840b11d6531d9e
ThisIsASL <1508560189@qq.com>
2015-03-29 18:17:18 +0800

    Initial Commit

---
+++ PublicComment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PublicComment
+//
+//  Created by Catherine on 15/3/29.
+//  Copyright (c) 2015年 Catherine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PublicComment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PublicComment
+//
+//  Created by Catherine on 15/3/29.
+//  Copyright (c) 2015年 Catherine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
